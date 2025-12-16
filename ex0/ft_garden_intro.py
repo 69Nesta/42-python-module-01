ab8309ee3d5ed3a8750262f64607e13ceef8ddeb
69Nesta <petitromeo74@gmail.com>
2025-12-16 10:34:20 +0100

    Feat: ex0 and ex1

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,13 @@
+def main():
+    name = "Nicola"
+    height = 42
+    age = 30
+    print(f"Plant : {name}\n" +
+          f"Height : {height} cm\n" +
+          f"Age : {age} days\n")
+
+
+if __name__ == '__main__':
+    print("=== Welcome to My Garden ===")
+    main()
+    print("=== End of Program ===")
---
+++ ex1/ft_garden_data.py
@@ -0,0 +1,20 @@
+class Plant:
+    def __init__(self, name: str, height: int, age: int):
+        self.name = name
+        self.height = height
+        self.age = age
+
+    def show(self) -> None:
+        print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+def main() -> None:
+    Plant("Rose", 25, 30).show()
+    Plant("Sunflower", 80, 40).show()
+    Plant("Cactus", 15, 120).show()
+
+
+if __name__ == "__main__":
+    print("===  Garden Plant Registry  ===")
+    main()
+    print("=== End of Program ===")
