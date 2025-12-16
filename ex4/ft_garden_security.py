667d352e2eca35675a8debd47a39ed9f5ec91697
69Nesta <petitromeo74@gmail.com>
2025-12-16 13:37:14 +0100

    Fix: ex4

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -2,35 +2,37 @@
 
 class SecurePlant:
     def __init__(self, name, height, age):
-        self.name = name
         print(f"Plant created: {name}")
-        self.set_height(height)
+        self.name = name
+        self.__age = 0
         self.set_age(age)
+        self.__height = 0
+        self.set_height(height)
 
     def set_age(self, age):
         if (age < 0):
             print(f"Invalid operation attempted: age {age} [REJECTED]")
         else:
             print(f"Age updated: {age} [OK]")
-            self._age = age
+            self.__age = age
 
     def set_height(self, height):
         if (height < 0):
             print(f"Invalid operation attempted: height {height}cm [REJECTED]")
         else:
             print(f"Height updated: {height}cm [OK]")
-            self._height = height
+            self.__height = height
 
     def get_age(self) -> int:
-        return (self._age)
+        return (self.__age)
 
     def get_height(self) -> int:
-        return (self._height)
+        return (self.__height)
 
 
 def ft_garden_security() -> None:
     print("=== Garden Security System ===")
-    plant = SecurePlant("Rose", 25, 30)
+    plant = SecurePlant("Rose", -25, -30)
     plant.set_height(-5)
     print(f"Current plant: {plant.name} ({plant.get_height()}cm" +
           f", {plant.get_age()} days)")
