a08633e8a0b1e605aceb03e7cd34095051e5c6e4
69Nesta <petitromeo74@gmail.com>
2026-01-05 12:27:22 +0100

    Feat: add SardenStats in GardenManager

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,21 +1,21 @@
 #! python3
 
 class Utility:
-    """Utility class for common helper methods."""
+    '''Utility class for common helper methods.'''
     @staticmethod
     def name_to_id(name: str) -> str:
-        """Convert a name to a standardized ID format."""
+        '''Convert a name to a standardized ID format.'''
         return name.lower().replace(' ', '_')
 
     @staticmethod
     def print_error(str: str) -> None:
-        """Print an error message and return None."""
+        '''Print an error message and return None.'''
         print("Error: " + str)
 
     @staticmethod
     def format_dict_int(values: dict[str, int]) -> str:
-        """Format a dictionary of string keys and integer values into a
-        readable string."""
+        '''Format a dictionary of string keys and integer values into a
+        readable string.'''
         formatted = ''
         index = 0
         for k, v in values.items():
@@ -27,148 +27,148 @@ class Utility:
 
 
 class Plant:
-    """Base class for plants in the garden."""
+    '''Base class for plants in the garden.'''
     def __init__(self, name: str, height: int) -> None:
-        """Initialize a Plant with a name and height."""
+        '''Initialize a Plant with a name and height.'''
         self._type = 'regular'
         self.set_name(name)
         self.set_height(height)
         self._grow = 0
 
     def set_name(self, name: str) -> None:
-        """Set the name of the plant, capitalizing each word."""
+        '''Set the name of the plant, capitalizing each word.'''
         if (name):
             self._name = name.title()
 
     def get_name(self) -> str:
-        """Get the name of the plant."""
+        '''Get the name of the plant.'''
         return self._name
 
     def set_height(self, height: int) -> None:
-        """Set the initial height of the plant."""
+        '''Set the initial height of the plant.'''
         if (height >= 0):
             self._height = height
         else:
             Utility.print_error('Height can\'t be negaive !')
 
     def get_height(self) -> int:
-        """Get the current height of the plant, including growth."""
+        '''Get the current height of the plant, including growth.'''
         return self._height + self.get_grow()
 
     def height_validation(self) -> bool:
-        """Validate if the current height matches initial height plus
-        growth."""
+        '''Validate if the current height matches initial height plus
+        growth.'''
         return self.get_grow() + self._height == self.get_height()
 
     def get_plant(self) -> str:
-        """Get a string representation of the plant."""
+        '''Get a string representation of the plant.'''
         return f'{self._name}: {self.get_height()}cm'
 
     def grow(self, grow=1) -> None:
-        """Increase the growth of the plant by a specified amount."""
+        '''Increase the growth of the plant by a specified amount.'''
         self._grow += grow
         print(f'{self._name} grew {grow}cm')
 
     def get_grow(self) -> int:
-        """Get the total growth of the plant."""
+        '''Get the total growth of the plant.'''
         return self._grow
 
     def get_type(self) -> str:
-        """Get the type of the plant."""
+        '''Get the type of the plant.'''
         return self._type
 
 
 class FloweringPlant(Plant):
-    """Class representing a flowering plant."""
+    '''Class representing a flowering plant.'''
     def __init__(self, name: str, height: int, color: str) -> None:
-        """Initialize a FloweringPlant with a name, height,
-        and flower color."""
+        '''Initialize a FloweringPlant with a name, height,
+        and flower color.'''
         super().__init__(name, height)
         self._type = 'flowering'
         self._blooming = True
         self.set_color(color)
 
     def set_color(self, color: str) -> None:
-        """Set the color of the flowers."""
+        '''Set the color of the flowers.'''
         if (color):
             self.color = color.lower()
 
     def toggle_blooming(self):
-        """Toggle the blooming state of the plant."""
+        '''Toggle the blooming state of the plant.'''
         self._blooming = not self._blooming
 
     def get_blooming(self) -> bool:
-        """Get the blooming state of the plant."""
+        '''Get the blooming state of the plant.'''
         return self._blooming
 
     def get_plant(self):
-        """Get a string representation of the flowering plant."""
+        '''Get a string representation of the flowering plant.'''
         blooming = ' (blooming)' if self._blooming else ''
         return super().get_plant() + f', {self.color} flowers{blooming}'
 
 
 class PrizeFlower(FloweringPlant):
-    """Class representing a prize flower with additional prize points."""
+    '''Class representing a prize flower with additional prize points.'''
     def __init__(self, name: str, height: int, color: str, prize: int) -> None:
-        """Initialize a PrizeFlower with a name, height, color,
-        and prize points."""
+        '''Initialize a PrizeFlower with a name, height, color,
+        and prize points.'''
         super().__init__(name, height, color)
         self._type = 'prize flowers'
         self.set_prize(prize)
 
     def set_prize(self, prize) -> None:
-        """Set the prize points for the prize flower."""
+        '''Set the prize points for the prize flower.'''
         self._prize = prize
 
     def get_prize(self) -> int:
-        """Get the prize points of the prize flower."""
+        '''Get the prize points of the prize flower.'''
         return self._prize
 
     def get_plant(self):
-        """Get a string representation of the prize flower."""
+        '''Get a string representation of the prize flower.'''
         return super().get_plant() + f', Prize Points: {self._prize}'
 
 
 class Garden:
-    """Class representing a garden containing various plants."""
+    '''Class representing a garden containing various plants.'''
     def __init__(self, name: str) -> None:
-        """Initialize a Garden with a name and an empty list of plants."""
+        '''Initialize a Garden with a name and an empty list of plants.'''
         self.set_name(name)
         self._plants: list[Plant | FloweringPlant | PrizeFlower] = []
 
     def set_name(self, name: str) -> None:
-        """Set the name of the garden, capitalizing each word."""
+        '''Set the name of the garden, capitalizing each word.'''
         if (name and not name.isspace()):
             self._name = name.title()
 
     def get_name(self) -> str:
-        """Get the name of the garden."""
+        '''Get the name of the garden.'''
         return self._name
 
     def get_plants(self):
-        """Get the list of plants in the garden."""
+        '''Get the list of plants in the garden.'''
         return self._plants
 
     def add(self, plant: Plant | FloweringPlant | PrizeFlower) -> None:
-        """Add a plant to the garden."""
+        '''Add a plant to the garden.'''
         print(f'Added {plant.get_name()} to {self.get_name()}\'s garden')
         self._plants.append(plant)
 
     def grow(self, grow=1) -> None:
-        """Help all plants in the garden grow by a specified amount."""
+        '''Help all plants in the garden grow by a specified amount.'''
         print(f'{self.get_name()} is helping all plants grow...')
         for plant in self._plants:
             plant.grow(grow)
 
     def get_growth(self) -> int:
-        """Get the total growth of all plants in the garden."""
+        '''Get the total growth of all plants in the garden.'''
         total_growth = 0
         for plant in self._plants:
             total_growth += plant.get_grow()
         return total_growth
 
     def get_types(self) -> dict[str, int]:
-        """Get a dictionary of plant types and their counts in the garden."""
+        '''Get a dictionary of plant types and their counts in the garden.'''
         types: dict[str, int] = {}
         for plant in self._plants:
             if (not types.get(plant.get_type())):
@@ -178,14 +178,14 @@ class Garden:
         return types
 
     def height_validation(self) -> str:
-        """Validate the height of all plants in the garden."""
+        '''Validate the height of all plants in the garden.'''
         for plant in self.get_plants():
             if (not plant.height_validation()):
                 return ('False')
         return ('True')
 
     def get_score(self) -> int:
-        """Calculate the score of the garden based on plant attributes."""
+        '''Calculate the score of the garden based on plant attributes.'''
         score = 0
         for plant in self.get_plants():
             score += plant.get_height()
@@ -196,19 +196,19 @@ class Garden:
         return score
 
     def print_stats(self) -> None:
-        """Print statistics about the plants in the garden."""
+        '''Print statistics about the plants in the garden.'''
         print(f'Plants added: {len(self._plants)}, '
               f'Total growth: {self.get_growth()}cm')
         print(f'Plants types: {Utility.format_dict_int(self.get_types())}')
 
     def print_plants(self) -> None:
-        """Print the list of plants in the garden."""
-        print(f'Plant{'s' if len(self.get_plants()) else ''} in garden:')
+        '''Print the list of plants in the garden.'''
+        print(f'Plant{"s" if len(self.get_plants()) else ""} in garden:')
         for plant in self.get_plants():
             print(f'- {plant.get_plant()}')
 
     def print_report(self) -> None:
-        """Print a comprehensive report of the garden."""
+        '''Print a comprehensive report of the garden.'''
         print(f'=== {self.get_name()} Garden Report ===')
         self.print_plants()
         print('')
@@ -218,13 +218,49 @@ class Garden:
 
 
 class GardenManager:
-    """Class to manage multiple gardens."""
+    '''Class to manage multiple gardens.'''
+    class GardenStats:
+        @staticmethod
+        def get_scores(gardens: dict[str, Garden]) -> str:
+            '''Get a string representation of the scores of all gardens.'''
+            scores = ''
+            index = 0
+            for k, v in gardens.items():
+                if (index != 0):
+                    scores += ', '
+                index += 1
+                scores += f'{v.get_name()} {v.get_score()}'
+            return scores
+
+        @staticmethod
+        def get_total_score(gardens: dict[str, Garden]) -> int:
+            '''Get the total score of all gardens in the network.'''
+            total_score = 0
+            for garden in gardens.values():
+                total_score += garden.get_score()
+            return total_score
+
+        @staticmethod
+        def print_stats(gardens: dict[str, Garden]) -> None:
+            '''Print statistics about all gardens in the network.'''
+            print(f'Gardens Score - {__class__.get_scores(gardens)}')
+            print(f'Total gardens score: {__class__.get_total_score(gardens)}')
+            print('Total plants managed: ' +
+                  f'{sum(len(g.get_plants()) for g in gardens.values())}')
+            print(f'Total gardens managed: {len(gardens)}')
+
     def __init__(self):
-        """Initialize a GardenManager with an empty network of gardens."""
+        '''Initialize a GardenManager with an empty network of gardens.'''
         self._network: dict[str, Garden] = {}
 
-    def create_garden_network(self, name) -> Garden:
-        """Create a new garden in the network."""
+    @classmethod
+    def create_garden_network(cls) -> 'GardenManager':
+        '''Create a new garden network manager.'''
+        print('Creating garden network...\n')
+        return cls()
+
+    def create_garden(self, name) -> Garden:
+        '''Create a new garden in the network.'''
         id = Utility.name_to_id(name)
         if (self._network.get(id)):
             return Utility.print_error("Name already taken!")
@@ -232,42 +268,30 @@ class GardenManager:
         return self._network[id]
 
     def get_garden(self, name) -> Garden:
-        """Retrieve a garden from the network by name."""
+        '''Retrieve a garden from the network by name.'''
         id = Utility.name_to_id(name)
         if (not self._network.get(id)):
             return Utility.print_error("Unknown garden!")
         return self._network[id]
 
+    def get_gardens(self) -> dict[str, Garden]:
+        '''Retrieve all gardens in the network.'''
+        return self._network
+
     def del_garden(self, name) -> None:
-        """Delete a garden from the network by name."""
+        '''Delete a garden from the network by name.'''
         id = Utility.name_to_id(name)
         if (not self._network.get(id)):
             return Utility.print_error("Unknown garden!")
         del self._network[id]
 
-    def get_scores(self) -> str:
-        """Get a string representation of the scores of all gardens."""
-        scores = ''
-        index = 0
-        for k, v in self._network.items():
-            if (index != 0):
-                scores += ', '
-            index += 1
-            scores += f'{v.get_name()} {v.get_score()}'
-        return scores
-
-    def print_stats(self) -> None:
-        """Print statistics about all gardens in the network."""
-        print(f'Gardens Score - {self.get_scores()}')
-        print(f'Total gardens managed: {len(self._network)}')
-
 
 def ft_garden_analytics():
-    """Demo function for the Garden Management System."""
+    '''Demo function for the Garden Management System.'''
     print('=== Garden Management System Demo ===\n')
-    garden_manager = GardenManager()
-    garden_manager.create_garden_network('aLicE')
-    bob_garden = garden_manager.create_garden_network('bOb')
+    garden_manager = GardenManager.create_garden_network()
+    garden_manager.create_garden('aLicE')
+    bob_garden = garden_manager.create_garden('bOb')
     bob_garden.add(Plant('Poppies', 10))
     print('')
     alice_garden = garden_manager.get_garden('AliCe')
@@ -278,7 +302,7 @@ def ft_garden_analytics():
     alice_garden.grow()
     print('')
     alice_garden.print_report()
-    garden_manager.print_stats()
+    garden_manager.GardenStats.print_stats(garden_manager.get_gardens())
 
 
 if __name__ == '__main__':
